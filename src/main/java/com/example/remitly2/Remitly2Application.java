fa450636b2000b5f5f58bc15fe3f32f1e31957f2
Pawel <pencak.pawel@gmail.com>
2025-04-04 14:32:31 +0100

    Starting se up

---
+++ src/main/java/com/example/remitly2/Remitly2Application.java
@@ -0,0 +1,13 @@
+package com.example.remitly2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Remitly2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Remitly2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/remitly2/Remitly2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.remitly2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Remitly2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
